82fde52b4368a5e20fefe735b43a00eb48ae585e
IngCristianJimenez <ingcrisjimenez@hotmail.com>
2026-06-05 20:36:02 -0600

    Actualizé el archivo Hello.py con la clase del día de hoy y agregué el archivo de git_commands

--- Hello.py
+++ Hello.py
@@ -1,1 +1,69 @@
 print ('Hello World')
+
+#Este es un comentario de una sola línea
+
+"""
+Este es un comentario
+de varias líneas
+"""
+
+#Python distingue entre máyusculas y minúsculas
+
+Variable = 1
+variable = 2
+VARIABLE = 3
+
+""" Todas las variables anteriores 
+son diferentes"""
+
+"""
+A diferencia de otros lenguajes. Python no requiere el uso de punto y coma (;)
+al final de cada instrucción. Sin embargo si se desea escribir varias instrucciones
+en una sola línea, se pueden separar con punto y coma.
+"""
+
+#Por ejemplo:
+
+instruccion1; instruccion2; instruccion3
+
+
+#Uso de paréntesis
+
+"""
+Los paréntesis se utilizan para agrupar expresiones, definir funciones y realizar llamadas a funciones.
+"""
+#Por ejemplo:
+
+resultado = (a + b) * c
+
+
+#Tipos de datos básicos
+
+Enteros = int
+
+#Ejemplos:
+
+edad = 25
+cantidad = 100
+
+Flotantes = float   # aquellos que tienen una parte decimal
+
+precio = 9.99
+altura = 1.75
+
+
+Cadenas_de_texto = strings
+
+"""
+Secuencias de caracteres encerradas entre comillas simples ('...') o dobles ("...").
+Se utilizan para representar texto en Python.
+"""
+
+nombre = "Juan"
+mensaje = '¡Hola Mundo!'
+
+
+# Puedes incluir caracteres especiales en las cadenas utilizando el carácter de escape \.
+# Por ejemplo, para incluir comillas dentro de una cadena, puedes usar \' o \". 
+# También puedes utilizar la notación de triple comilla ('''...''' o """...""") 
+# para crear cadenas de varias líneas.

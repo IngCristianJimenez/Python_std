b6f6ccbc91eb7fd2ed3369c8e298f16609a5daea
IngCristianJimenez <ingcrisjimenez@hotmail.com>
2026-06-07 15:04:55 -0600

    Today class...

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ en una sola línea, se pueden separar con punto y coma.
 
 #Por ejemplo:
 
-instruccion1; instruccion2; instruccion3
+# instruccion1; instruccion2; instruccion3
 
 
 #Uso de paréntesis
@@ -34,7 +34,7 @@ Los paréntesis se utilizan para agrupar expresiones, definir funciones y realiz
 """
 #Por ejemplo:
 
-resultado = (a + b) * c
+# resultado = (a + b) * c
 
 
 #Tipos de datos básicos
@@ -52,7 +52,7 @@ precio = 9.99
 altura = 1.75
 
 
-Cadenas_de_texto = strings
+# Cadenas_de_texto = strings
 
 """
 Secuencias de caracteres encerradas entre comillas simples ('...') o dobles ("...").
@@ -133,3 +133,59 @@ a = b = c = 10
 #    modulo = a % b   # 1
 #    exponenciacion = a ** b   # 1000
 
+
+""" 
+De comparación
+
+Los operadores de comparación se utilizan para comparar dos valores y devuelven un valor booleano (True o False)
+según el resultado de la comparación.
+
+"""
+
+# Los operadores de comparación en Python son:
+# 
+# Igual a (==): devuelve True si ambos valores son iguales.
+# Diferente de (!=): devuelve True si los valores son diferentes.
+# Mayor que (>): devuelve True si el primer valor es mayor que el segundo.
+# Menor que (<): devuelve True si el primer valor es menor que el segundo.
+# Mayor o igual que (>=): devuelve True si el primer valor es mayor o igual que el segundo.
+# Menor o igual que (<=): devuelve True si el primer valor es menor o igual que el segundo.
+
+
+# Ejemplos:
+
+# a = 10
+# b = 3
+
+# igual = a == b   # False
+# diferente = a != b   # True
+# mayor que = a > b   # True
+# menor que = a < b   # False
+# mayor o igual = a >= b   # True
+# menor o igual = a <= b   # False
+
+
+"""
+Lógicos
+
+Los operadores lógicos se utilizan para combinar expresiones condicionales y evaluar múltiples condiciones.
+
+"""
+
+# Los operadores lógicos en Python son:
+
+# AND (and): devuelve True si ambas condiciones son verdaderas.
+# OR (or): devuelve True si al menos una de las condiciones es verdadera.
+# NOT (not): invierte el valor de una condición, devuelve True si la condición es falsa y False si la condición es verdadera.
+
+"""Ejemplo:
+
+a = 10
+b = 3
+
+
+resultado_and = (a > 5) and (b < 5)   # True
+resultado_or = (a > 15) or (b < 5)   # True
+resultado_not = not (a > 5)   # False
+
+"""

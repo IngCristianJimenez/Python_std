76e2a6436c34b689c8fcc6f208a5d78f78b2e303
IngCristianJimenez <ingcrisjimenez@hotmail.com>
2026-06-12 18:44:03 -0600

    Types of lists & their commands

--- Hello.py
+++ Hello.py
@@ -440,3 +440,82 @@ En este ejemplo, el bucle for itera sobre los números del 0 al 4,
 pero no se realiza ninguna acción dentro del bucle debido a la instrucción pass.
 Esto puede ser útil cuando se está desarrollando un programa y se desea reservar un bloque de código para implementarlo más adelante.
 """
+
+
+print ("\n 4.- Estructuras de Datos")
+
+"""
+Las estructuras de datos nos permiten organizar y almacenar datos de manera eficiente en nuestros programas.
+Python proporciona varias estructuras de datos integradas, como listas, tuplas, diccionarios y conjuntos,
+cada una con sus propias características y usos.
+"""
+
+# *** Listas ***
+
+"""
+Una lista es una estructura de datos mutable y ordenada que permite almacenar una colección de elementos.
+Los elementos de una lista pueden ser de diferentes tipos de datos y se encierran entre corchetes [], separados por comas.
+"""
+
+# Creación y acceso
+
+# Para crear una lista, simplemente encierra los elementos entre corchetes:
+
+frutas = ["manzana", "banana", "naranja"]
+
+print ("\n Lista 0,1,2\n")
+print (frutas[0]) # Imprime manzana
+print (frutas[1]) # Imprime banana
+print (frutas[2]) # Imprime naranja
+
+
+"""
+También puedes acceder a los elementos desde el final de la lista utilizando índices negativos.
+El índice -1 representa el último elemento, -2 representa el penúltimo, y así sucesivamente.
+"""
+print ("\n Lista -1,-2 y -3\n")
+print (frutas[-1]) # Imprime naranja
+print (frutas[-2]) # Imprime banana
+print (frutas[-3]) # Imprime manzana
+
+print("\n Métodos de listas")
+
+# Las listas en Python tienen varios métodos incorporados que nos permiten manipular y modificar los elementos de la lista.
+
+# Algunos métodos comunes son:
+
+"""
+append(elemento): agrega un elemento al final de la lista.
+insert(indice, elemento): inserta un elemento en una posición específica de la lista.
+remove(elemento): elimina la primera aparición de un elemento en la lista.
+pop(indice): elimina y devuelve el elemento en una posición específica de la lista.
+sort(): ordena los elementos de la lista en orden ascendente.
+reverse(): invierte el orden de los elementos en la lista.
+"""
+
+frutas = ["manzana", "banana", "naranja"]
+
+
+frutas.append("pera")
+print(frutas)  # Imprime ["manzana", "banana", "naranja", "pera"]
+
+
+frutas.insert(1, "uva")
+print(frutas)  # Imprime ["manzana", "uva", "banana", "naranja", "pera"]
+
+
+frutas.remove("banana")
+print(frutas)  # Imprime ["manzana", "uva", "naranja", "pera"]
+
+
+fruta_eliminada = frutas.pop(2)
+print(frutas)  # Imprime ["manzana", "uva", "pera"]
+print(fruta_eliminada)  # Imprime "naranja"
+
+
+frutas.sort()
+print(frutas)  # Imprime ["manzana", "pera", "uva"]
+
+
+frutas.reverse()
+print(frutas)  # Imprime ["uva", "pera", "manzana"]

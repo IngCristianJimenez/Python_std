76891079fd1e3ab99896c52b23902ab9779f1838
IngCristianJimenez <ingcrisjimenez@hotmail.com>
2026-06-13 23:55:32 -0600

    Tuples on Python

--- Hello.py
+++ Hello.py
@@ -519,3 +519,77 @@ print(frutas)  # Imprime ["manzana", "pera", "uva"]
 
 frutas.reverse()
 print(frutas)  # Imprime ["uva", "pera", "manzana"]
+
+
+# ***Listas de comprensión***
+
+# Las listas de comprensión son una forma concisa de crear nuevas listas basadas en una secuencia existente.
+# Permiten filtrar y transformar los elementos de una lista en una sola línea de código.
+
+# nueva_lista = [expresion for elemento in secuencia if condicion]
+
+
+# Ejemplo:
+
+numeros = [1, 2, 3, 4, 5]
+cuadrados = [x ** 2 for x in numeros if x % 2 == 0]
+print(cuadrados)  # Imprime [4, 16]
+
+# En este ejemplo, se crea una nueva lista llamada cuadrados, que contiene los cuadrados de los números pares de la lista numeros.
+# La expresión x ** 2 eleva cada elemento al cuadrado, y la condición if x % 2 == 0 filtra solo los números pares.
+
+
+
+
+# *** Tuplas ***
+
+"""
+Una tupla es una estructura de datos inmutable y ordenada que permite almacenar una colección de elementos.
+Los elementos de una tupla se encierran entre paréntesis (), separados por comas.
+"""
+
+# Creación y acceso
+
+
+# Para crear una tupla, encierra los elementos entre paréntesis:
+
+punto = (3, 4)
+
+#Para acceder a los elementos de una tupla, utiliza el índice del elemento entre corchetes, similar a las listas:
+
+print(punto[0])  # Imprime 3
+
+print(punto[1])  # Imprime 4
+
+"""
+A diferencia de las listas, las tuplas son inmutables, lo que significa que no se pueden modificar una vez creadas.
+No se pueden agregar, eliminar o cambiar elementos en una tupla existente.
+
+Las tuplas son útiles cuando necesitas almacenar una colección de elementos que no deben modificarse,
+como coordenadas o datos de configuración.
+"""
+
+
+
+# *** Métodos de tuplas ***
+
+"""
+Aunque las tuplas son inmutables, Python proporciona varios métodos útiles para trabajar con ellas:
+
+- count(elemento): devuelve el número de veces que aparece un elemento en la tupla. 
+
+- index(elemento): devuelve el índice de la primera aparición de un elemento en la tupla. Opcionalmente,
+se puede especificar el inicio y fin de la búsqueda. 
+
+- len(tupla): aunque no es un método de tupla propiamente dicho, esta función incorporada devuelve la longitud de la tupla.
+
+"""
+
+mi_tupla = (1, 2, 3, 2, 4, 2)
+
+
+print (mi_tupla.index(2))   # Salida: 1
+
+print (mi_tupla.index(2, 2))   #Salida: 3
+
+print (mi_tupla.index(2, 2, 4))   #Salida: 3
